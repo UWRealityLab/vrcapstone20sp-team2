5af14435dbab1659e3615906529042b3733f2fda
kss@Desktop <wangty.kss@gmail.com>
2020-04-23 15:35:07 -0700

    Lab_Scene upload

---
+++ Vac.Inc/Assets/Scenes/Lab_Scene/Scripts/Grab_1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem
+{
+  public class Grab_1 : MonoBehaviour
+  {
+      [EnumFlags]
+      public Hand.AttachmentFlags attachmentFlags = Hand.AttachmentFlags.ParentToHand | Hand.AttachmentFlags.DetachFromOtherHand;
+
+      private void HandHoverUpdate(Hand hand)
+      {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        if (startingGrabType != GrabTypes.None)
+        {
+          hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
+        }
+
+      }
+
+      private void HandAttachedUpdate(Hand hand)
+      {
+        if (hand.IsGrabEnding(gameObject))
+        {
+          hand.DetachObject(gameObject);
+        }
+      }
+      // Start is called before the first frame update
+      void Start()
+      {
+
+      }
+
+      // Update is called once per frame
+      void Update()
+      {
+
+      }
+  }
+}
