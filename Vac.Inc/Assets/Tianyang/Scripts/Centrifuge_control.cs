5896483e8a4ccc4a9ac603576dd710eb8c701d48
kss@Desktop <wangty.kss@gmail.com>
2020-04-29 00:13:07 -0700

    Animate centrifuge

---
+++ Vac.Inc/Assets/Tianyang/Scripts/Centrifuge_control.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem
+{
+    public class Centrifuge_control : MonoBehaviour
+    {
+
+        public HoverButton hoverButton;
+
+        private Animator _animator;
+
+        void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            hoverButton.onButtonDown.AddListener(OnButtonDown);
+        }
+
+        private void OnButtonDown(Hand hand)
+        {
+            _animator.SetBool("capOpen", !_animator.GetBool("capOpen"));
+        }
+    }
+}
