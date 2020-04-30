43a79c2790c3383064dee2973ad33362fa720a08
Fishfishz <yyu18@uw.edu>
2020-04-30 00:57:34 -0700

    Burner update
    control burner with a button.

---
+++ Vac.Inc/Assets/Scenes/Lab_Scene/Scripts/fireStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+  public class fireStart : MonoBehaviour
+  {
+      // Start is called before the first frame update
+      public ParticleSystem particleObject;
+
+      void Start()
+      {
+          particleObject.Stop();
+      }
+  }
+}
