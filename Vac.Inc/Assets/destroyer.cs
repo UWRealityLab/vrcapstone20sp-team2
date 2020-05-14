015d2b86a74673f6b5f9713b13353e1bbed3c562
Fishfishz <yyu18@uw.edu>
2020-05-14 00:13:57 -0700

    trash can update
    trash can update

---
+++ Vac.Inc/Assets/destroyer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyer : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+      if(other.gameObject.tag == "Ctube"){
+        Debug.Log("dedededede");
+        Destroy(other.gameObject);
+      }
+    }
+}
