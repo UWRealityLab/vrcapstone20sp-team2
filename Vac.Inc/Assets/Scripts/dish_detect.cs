05387b5ad38210f371831c04f9be60ecdf8fe223
Fishfishz <yyu18@uw.edu>
2020-05-21 02:33:58 -0700

    petriDish and Incubator
    petriDish and Incubator

---
+++ Vac.Inc/Assets/Scripts/dish_detect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dish_detect : MonoBehaviour
+{
+  private LiquidFillManager liquid;
+
+  void OnTriggerEnter(Collider other)
+  {
+      if (other.gameObject.tag == "Beaker")
+      {
+          liquid = other.transform.gameObject.GetComponent<LiquidFillManager>();
+          liquid.IncubateLiquid();
+          print("!!!!!!!!!");
+      }
+  }
+}
