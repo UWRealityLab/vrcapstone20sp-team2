1f2959402d8188dc0b985c0f75cce3c9ce7f29bc
kss@Desktop <wangty.kss@gmail.com>
2020-06-09 18:56:18 -0700

    Update dish_detect.cs

--- Vac.Inc/Assets/Scripts/dish_detect.cs
+++ Vac.Inc/Assets/Scripts/dish_detect.cs
@@ -12,7 +12,6 @@ public class dish_detect : MonoBehaviour
       {
           liquid = other.transform.gameObject.GetComponent<LiquidFillManager>();
           liquid.IncubateLiquid();
-          print("!!!!!!!!!");
       }
   }
 }
